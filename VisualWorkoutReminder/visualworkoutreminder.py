e5988a6883fc4932f3a8902848e39fcd854bafe9
gkjt <georgetucker1@hotmail.co.uk>
2015-02-21 21:18:04 +0000

    Fixed syntax errors

--- VisualWorkoutReminder/visualworkoutreminder.py
+++ VisualWorkoutReminder/visualworkoutreminder.py
@@ -5,26 +5,29 @@ import subprocess
 import os
 import random
 
-run = true
+run = True
 
 
-//using http://www.sirentuan.com/3780124/codep1/python-open-pdf-file-to-specific-pagesection
-def openWorkout()
+
+#using http://www.sirentuan.com/3780124/codep1/python-open-pdf-file-to-specific-pagesection
+def openWorkout():
 	path_to_pdf = os.path.abspath('.\100-no-equipment-workouts-2014-by-neilarey.pdf')
 	path_to_acrobat = os.path.abspath('C:\Program Files (x86)\Adobe\Reader 10.0\Reader\AcroRd32.exe') 
 	
-	page = random.randint(10, 100, 2)
+	page = random.randrange(10, 100, 2)
 	
-	process = subprocess.Popen([path_to_acrobat, '/A', 'page=' + page, path_to_pdf], shell=False, stdout=subprocess.PIPE)
+	process = subprocess.Popen([path_to_acrobat, '/A', 'page=' + str(page), path_to_pdf], shell=False, stdout=subprocess.PIPE)
 	process.wait()
 
 
 while run:
 	now = datetime.datetime.today()
-	nextWorkout = datetime.datetime(t.year,t.month,t.day,16,30)
-	if(now.hour >= 16)
-		nextWorkout += time.timeDelta(days=1)
+	nextWorkout = now #datetime.datetime(now.year, now.month, now.day, 19, 22)
+	#if(now.hour >= 16):
+		#nextWorkout += datetime.timedelta(days=0)
+	print((nextWorkout-now).seconds)
 	time.sleep((nextWorkout-now).seconds)
 	openWorkout()
-	//DEBUG:
-	run = false
+	#DEBUG:
+	run = False
+	
