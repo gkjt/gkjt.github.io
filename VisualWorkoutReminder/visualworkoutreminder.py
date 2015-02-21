8a9bd0b49b99b2b1dc8720a9234219184889a99d
gkjt <georgetucker1@hotmail.co.uk>
2015-02-21 21:56:44 +0000

    Scrapped using a subprocess with a pdf, instead using the website

--- VisualWorkoutReminder/visualworkoutreminder.py
+++ VisualWorkoutReminder/visualworkoutreminder.py
@@ -1,7 +1,7 @@
 import time
 import datetime
 import cmd
-import subprocess
+import webbrowser
 import os
 import random
 
@@ -11,20 +11,15 @@ run = True
 
 #using http://www.sirentuan.com/3780124/codep1/python-open-pdf-file-to-specific-pagesection
 def openWorkout():
-	path_to_pdf = os.path.abspath('.\100-no-equipment-workouts-2014-by-neilarey.pdf')
-	path_to_acrobat = os.path.abspath('C:\Program Files (x86)\Adobe\Reader 10.0\Reader\AcroRd32.exe') 
+	path_to_site = "http://neilarey.com/workouts.html"
+	webbrowser.open(path_to_site, new=0, autoraise=True)
 	
-	page = random.randrange(10, 100, 2)
-	
-	process = subprocess.Popen([path_to_acrobat, '/A', 'page=' + str(page), path_to_pdf], shell=False, stdout=subprocess.PIPE)
-	process.wait()
-
 
 while run:
 	now = datetime.datetime.today()
-	nextWorkout = now #datetime.datetime(now.year, now.month, now.day, 19, 22)
-	#if(now.hour >= 16):
-		#nextWorkout += datetime.timedelta(days=0)
+	nextWorkout = datetime.datetime(now.year, now.month, now.day, 16, 30)
+	if(now >= nextWorkout):
+		nextWorkout += datetime.timedelta(days=1)
 	print((nextWorkout-now).seconds)
 	time.sleep((nextWorkout-now).seconds)
 	openWorkout()
